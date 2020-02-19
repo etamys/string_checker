c71edd5fdf7c1058da07855f807e413cd4d1706c
Yogesh  singh <57357736+etamys@users.noreply.github.com>
2020-02-19 20:08:54 +0530

    Update easypalindrome_expression.cpp

--- easypalindrome_expression.cpp
+++ easypalindrome_expression.cpp
@@ -2,74 +2,51 @@
 using namespace std;
 int main()
 {
-	char str[20];
-	int i=0,n;
+	char str[20],word[20];
+	int i=0,j=0,c,n,f;
+	c=0;
+	f=1;
 	while((str[i]=getchar())!='\n')
    {
+   	   if(str[i]=='{' || str[i]=='}' || str[i]=='(' || str[i]==')' ||
+   	   	  str[i]=='<'||str[i]=='>' || str[i]== '[' || str[i]==']')
+   	   {
+   	   	word[j]=str[i];
+   	   	j++;
+   	   	c++;
+   	   }
 	    ++i;
    }
-   n=i;
-   i=0;
-   while(i<n) 
-   {
-   	   if((-str[i]>='a' && str[i]<='z') || (str[i]=='+' || str[i]=='-'))
-       {
-
-       	for(int j=i;j<n;j++)
-       	{
-
-       	  str[j]=str[j+1];
-        }
-        --n;
-      }
-        
-       if((str[i]>='a' && str[i]<='z') || (str[i]>='0' && str[i]<+'9'))
-        {
-          for(int j=i;j<n;j++)
-       	  {
-
-       	  str[j]=str[j+1];
-          }
-          --n;
-        }
-       if(str[i]>='a' && str[i]<='z')
-        {
-          for(int j=i;j<n;j++)
-       	  {
-
-       	  str[j]=str[j+1];
-          }
-          --n;
-        }
-       ++i;  
-   }
-   n=i;
-
-   //now check string
-   int k,l,f=0;
+   n=c;
+j=0;
+cout<<n;
    if(n>2)
-  {
-     for((k=0,l=n-1);k<=n/2;++k,--l) 
+{
+     for((i=0,j=n-1);i<n/2;++i,--j) 
      {
-    	if((str[k]=='{' && str[l]=='{') ||
-    	   (str[k]=='(' && str[l]==')') ||
-    	   (str[k]=='[' && str[l]==']') ||
-    		(str[k]=='<' && str[l]=='>'))
+    	if((word[i]=='{' && word[j]=='{') ||
+    	  (word[i]=='(' && word[j]==')') ||
+    	  (word[i]=='[' && word[j]==']') ||
+    	  (word[i]=='<' && word[j]=='>'))
+
     	{
-    	  f=1;	
+    		f=1;
     	}
+    	else{f=0;}
     	
-      
+    
      }
-  }
-  if(f==1)
+
+    //now that's the time to display the message
+    if(f==1)
     {
     	cout<<"YES";
-    	
     }
     else
     {
     	cout<<"NO";
     }
+  
    
+}
 }
